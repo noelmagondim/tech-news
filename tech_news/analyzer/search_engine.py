584a6a307d3f6de87f0cdde1ee4d176f962df5e8
noelmagondim <noelmagondim@gmail.com>
2022-11-09 15:52:19 -0300

    Req 06: concluded

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_list = search_news({"title": {"$regex": title, "$options": 'i'}})
+    return [
+        (news["title"], news["url"])
+        for news in news_list
+    ]
 
 
 # Requisito 7
