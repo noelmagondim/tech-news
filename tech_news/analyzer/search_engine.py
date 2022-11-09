5c86c3379af166b26ccfcf507bb4b49e6d1ac475
noelmagondim <noelmagondim@gmail.com>
2022-11-09 16:01:29 -0300

    Req 07: concluded

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -12,7 +13,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        format_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        new_list = search_news({
+            "timestamp": format_date
+        })
+        return [
+            (news["title"], news["url"])
+            for news in new_list
+        ]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
