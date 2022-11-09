ee2611a386fe9273587442dc23d87bca3735551e
noelmagondim <noelmagondim@gmail.com>
2022-11-09 15:31:43 -0300

    Req 04: concluded

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,20 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return {
+        "url": selector.css("link[rel='canonical']::attr(href)").get(),
+        "title": selector.css("h1.entry-title::text").get().strip(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("span.author a::text").get(),
+        "comments_count": len(selector.css("comment-list").getall()),
+        "summary": "".join(selector.css(
+            "div.entry-content > p:nth-of-type(1) *::text"
+            ).getall()
+        ).strip(),
+        "tags": selector.css("a[rel='tag']::text").getall(),
+        "category": selector.css("span.label::text").get()
+    }
 
 
 # Requisito 5
